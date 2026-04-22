9cf6114accea235b2663eb191577bac17cc3b509
Felipe9810 <felipe.marinho.lima@hotmail.com>
2026-04-22 09:23:58 -0300

    Felipe.Jsx

---
+++ src/components/Felipe.jsx
@@ -0,0 +1,14 @@
+
+const Felipe = () => {
+  return (
+    <div>
+      <ul>
+        <li>Felipe</li>
+        <li>felipe.teste@hotmail.com</li>
+        <li>desenvolvedor fullstack</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Felipe
